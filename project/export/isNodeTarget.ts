081a3293844c73c2593f9f64c10f1ad93ee7d9ae
eXory2024 <exory@anio.software>
2025-07-06 22:41:01 +0200

    Add support for js-hybrid-lite.

--- project/export/isNodeTarget.ts
+++ project/export/isNodeTarget.ts
@@ -1,9 +1,10 @@
 import type {TargetIdentifier} from "@anio-software/enkore-private.spec/primitives"
 
 export function isNodeTarget(target: TargetIdentifier): boolean {
-	if (target === "js-node"    ||
-	    target === "js-hybrid"  ||
-//	    target === "jsx-node"   ||
+	if (target === "js-node"        ||
+	    target === "js-hybrid"      ||
+	    target === "js-hybrid-lite" ||
+//	    target === "jsx-node"       ||
 	    target === "jsx-hybrid"   ) {
 		return true
 	}
--- project/export/isWebTarget.ts
+++ project/export/isWebTarget.ts
@@ -1,9 +1,10 @@
 import type {TargetIdentifier} from "@anio-software/enkore-private.spec/primitives"
 
 export function isWebTarget(target: TargetIdentifier): boolean {
-	if (target === "js-web"     ||
-	    target === "js-hybrid"  ||
-	    target === "jsx-web"    ||
+	if (target === "js-web"         ||
+	    target === "js-hybrid"      ||
+	    target === "js-hybrid-lite" ||
+	    target === "jsx-web"        ||
 	    target === "jsx-hybrid"   ) {
 		return true
 	}
